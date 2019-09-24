90fc28e5b0fe9877714fe0e940c3273de62bd836
steinhakasevs <stein.hakase.vs@gmail.com>
2019-09-23 21:43:37 -0600

    updated the signup interface with the new decorator validator emailShouldNotExist

--- src/resolvers/userResolvers/signUpInterface.ts
+++ src/resolvers/userResolvers/signUpInterface.ts
@@ -1,6 +1,6 @@
 import { IsEmail } from "class-validator";
 import { ArgsType, Field } from "type-graphql";
-import { emailAlreadyExists } from "../customValidators/emailAlreadyExists";
+import { emailShouldNotExist } from "../customValidators/emailShouldNotExist";
 
 // Sign up interface
 @ArgsType()
@@ -13,7 +13,7 @@ export default class signUpInterface {
 
   @Field({ nullable: false })
   @IsEmail(undefined, { message: 'Email is invalid. Please use a valid email' })
-  @emailAlreadyExists({ message: 'Email already in use' })
+  @emailShouldNotExist({ message: 'Email already in use' })
   email: string
 
   @Field({ nullable: false })
