b140f220be428a33c658a7178313789324643fd9
steinhakasevs <stein.hakase.vs@gmail.com>
2019-09-23 21:44:26 -0600

    Created isAuth function for middleware

---
+++ src/resolvers/middlewares/isAuth.ts
@@ -0,0 +1,28 @@
+import { context } from '../../models'
+import { MiddlewareFn } from 'type-graphql'
+import { AuthenticationError } from 'apollo-server-core'
+import jwt from 'jsonwebtoken'
+
+const { TOKEN_SECRET } = process.env
+
+export const isAuth: MiddlewareFn<context> = async ({ context }, next) => {
+  let authHeader = context.req.headers['authorization']
+
+  try {
+    // Return an error if the authorization header is missing
+    if (!authHeader) { throw new AuthenticationError('Please provide a valid authorization header') }
+    
+    // Isolate the token
+    const token = authHeader.split(' ')[1]
+
+    // Verify the token with JWT and extract the payload
+    let payload = jwt.verify(token, TOKEN_SECRET!)
+
+    // If theres a payload insert it to the context
+    context.payload = payload as any
+  } catch (e) {
+    console.log(e)
+    throw new AuthenticationError("Authorization Error")
+  }
+  return next()
+}
