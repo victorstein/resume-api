93b26d1bceb65d4647a57cdd10f0ad55a307bf7a
steinhakasevs <stein.hakase.vs@gmail.com>
2019-09-23 21:41:38 -0600

    Created the context interface

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,12 @@
 import { userModel } from './user'
+import { Request, Response } from 'express'
+
+export interface context {
+  userModel: any
+  req: Request
+  res: Response
+  payload?: { id: string }
+}
 
 export default {
   userModel
